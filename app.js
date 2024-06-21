291066fafc27c6e5d96a7470ca8344ceed32765b
githubscientist <sathish.datascientist@gmail.com>
2024-06-21 13:06:14 +0530

    mongodb --get method --users

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models/user');
 
 const app = express();
 
 app.use(express.json());
 
-
+app.get('/users', async (request, response) => {
+    try {
+        const users = await User.find({}, {_id: 0, password: 0});
+        response.status(200).json(users);
+    } catch (error) {
+        response.status(500).send({ message: error.message });
+    }
+})
 
 module.exports = app;
